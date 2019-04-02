9313f5773eeadb6bd8b5d699ca1540bc49a8de69
Lauri Suoknuuti <l.suoknuuti@hotmail.com>
2019-04-02 10:53:31 +0300

    updated bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,14 @@
 import discord
+
+token = "NTYyMzYxMTMyNjY1Mjc0Mzcx.XKJuvw.Nyn-SaliCD6S_IbR6lqfSrLOwYk"
+
+client = discord.Client()
+id = client.get_guild(330741882147700736)
+
+
+@client.event
+async def on_message(message):
+    if message.content.find("hi") != -1:
+        await message.channel.send("hello")
+
+client.run(token)
